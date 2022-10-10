e5775262b790f2af0ce09774019b4a54df382a35
unknown <Ashwini1798>
2022-10-10 06:02:20 -0700

    first commit

---
+++ Introduction_to_Javascript/problem2.js
@@ -0,0 +1,6 @@
+var name="Ashwini";
+console.log(name);
+var Fathers_name="Sharad";
+console.log(Fathers_name);
+var Mothers_name="Meena";
+console.log(Mothers_name);
---
+++ Introduction_to_Javascript/problem4.js
@@ -0,0 +1,28 @@
+
+var o="!  (\_/)                                     !";
+var p="! (='.'=)  ";
+var q="!  ()_()                                    !";
+var a="! Student Name : Ashwini                    !";
+var b="! RAYAN INTERNATIONAL SCHOOL                !";
+var c="! Grade : 5 |";
+var d="! Section : A |";
+var e="! Roll No : 3 !";
+var f="! Math:49/50";
+var g="! Physics:45/50";
+var h="! Biology:48/50!";
+console.log("._._._._._._._._._._._._._._._._._._._._._._.");
+console.log("!                                           !")
+console.log(o);
+
+console.log(p+"  "+"ℝ𝔼ℙ𝕆ℝ𝕋 ℂ𝔸ℝ𝔻                    !");
+console.log(q);
+
+console.log("!                                           !")
+console.log(b)
+console.log("!                                           !")
+console.log(a)
+console.log("!                                           !")
+console.log(c, d ,e);
+console.log("!                                           !")
+console.log(f+" "+g+" "+h);
+console.log("!_._._._._._._._._._._._._._._._._._._._._._!");
